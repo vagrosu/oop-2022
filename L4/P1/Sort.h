7cb4ff3772a0b6936a728e83268fe5e70842cd0a
Victor Grosu <vagrosu@yahoo.com>
2022-03-17 21:40:53 +0200

    P1 add

---
+++ L4/P1/Sort.h
@@ -0,0 +1,13 @@
+class Sort
+{
+
+    // add data members
+public:
+    // add constuctors
+    void InsertSort(bool ascendent=false);
+    void QuickSort(bool ascendent=false);
+    void BubbleSort(bool ascendent=false);
+    void Print();
+    int  GetElementsCount();
+    int  GetElementFromIndex(int index);
+}
